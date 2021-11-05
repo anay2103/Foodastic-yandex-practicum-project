009f7435b8e4e438420a3879065b46c7c8e8ad4d
anay2103 <55763427+anay2103@users.noreply.github.com>
2021-11-05 21:42:59 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -4,9 +4,9 @@ AUTH_USER_MODEL = 'users.MyUser'
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-DEBUG = os.environ.get('DEBUG', True)
+DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '130.193.48.28').split(' ')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'foodastic.co.vu').split(' ')
 
 SECRET_KEY = os.environ.get(
     'SECRET_KEY',
